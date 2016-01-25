ba9dec773b5bea24ed12c55578f2159046eb49bd
hang.song <hang.song@gmail.com>
2016-01-25 22:37:39 +0800

    cc
    cc

--- src/main/java/demo/SmackClient.java
+++ src/main/java/demo/SmackClient.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.SmackConfiguration;
 import org.jivesoftware.smack.filter.PacketFilter;
 import org.jivesoftware.smack.java7.Java7SmackInitializer;
 import org.jivesoftware.smack.packet.IQ;
@@ -12,8 +13,15 @@ import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 import org.jivesoftware.smack.util.StringUtils;
 import org.jivesoftware.smackx.disco.ServiceDiscoveryManager;
+import org.jivesoftware.smackx.filetransfer.FileTransferManager;
+import org.jivesoftware.smackx.filetransfer.OutgoingFileTransfer;
 import org.jivesoftware.smackx.iqregister.AccountManager;
 import org.jivesoftware.smackx.iqregister.packet.Registration;
+import org.jivesoftware.smackx.muc.DiscussionHistory;
+import org.jivesoftware.smackx.muc.HostedRoom;
+import org.jivesoftware.smackx.muc.MultiUserChat;
+import org.jivesoftware.smackx.muc.MultiUserChatManager;
+import org.jivesoftware.smackx.search.ReportedData;
 import org.jivesoftware.smackx.search.UserSearchManager;
 import org.jivesoftware.smackx.vcardtemp.packet.VCard;
 import org.jivesoftware.smackx.xdata.Form;
@@ -43,7 +51,10 @@ public class SmackClient {
         }
         return connection;
     }
-
+    /**
+     * 初始化连接
+     * @return
+     */
     public void openConnection(){
         try {
             if(null == connection || !connection.isAuthenticated()){
@@ -58,7 +69,10 @@ public class SmackClient {
             e.printStackTrace();
         }
     }
-
+    /**
+     * 关闭连接
+     * @return
+     */
     public void closeConnection(){
         if(connection!=null){
             if(connection.isConnected()){
@@ -68,7 +82,10 @@ public class SmackClient {
             connection = null;
         }
     }
-
+    /**
+     * 登陆
+     * @return
+     */
     public boolean login(String account, String password){
         try {
             if(connection==null){
@@ -86,7 +103,10 @@ public class SmackClient {
         }
         return false;
     }
-
+    /**
+     * 注册用户
+     * @return
+     */
     public boolean regist(String account, String password){
         if(getConnection()==null){
             return false;
@@ -118,7 +138,10 @@ public class SmackClient {
 
         return false;
     }
-
+    /**
+     * 更改用户状态
+     * @return
+     */
     public void setPresence(int code){
         XMPPTCPConnection conn = getConnection();
         if(conn == null){
@@ -178,7 +201,10 @@ public class SmackClient {
             e.printStackTrace();
         }
     }
-
+    /**
+     * 获取所有组
+     * @return
+     */
     public List<RosterGroup> getGroups(){
         if(getConnection() == null){
             return null;
@@ -194,7 +220,10 @@ public class SmackClient {
         System.out.println("get roster "+groupList);
         return groupList;
     }
-
+    /**
+     * 添加一个分组
+     * @return
+     */
     public boolean addGroup(String groupName){
         if(getConnection() == null){
             return false;
@@ -210,7 +239,10 @@ public class SmackClient {
 
         return false;
     }
-
+    /**
+     * 删除分组
+     * @return
+     */
     public boolean removeGroup(String groupName){
         try {
             Roster roster = Roster.getInstanceFor(getConnection());
@@ -220,7 +252,10 @@ public class SmackClient {
             return false;
         }
     }
-
+    /**
+     * 获取某个分组里面的所有好友
+     * @return
+     */
     public List<RosterEntry> getEntriesByGroup(String groupName){
         List<RosterEntry> entryList = new ArrayList<RosterEntry>();
 
@@ -236,7 +271,10 @@ public class SmackClient {
         System.out.println("group: "+groupName+" users: "+entryList);
         return entryList;
     }
-
+    /**
+     * 获取所有好友信息
+     * @return
+     */
     public List<RosterEntry> getAllEntries(){
         List<RosterEntry> rosterEntries = new ArrayList<RosterEntry>();
 
@@ -249,7 +287,10 @@ public class SmackClient {
 
         return rosterEntries;
     }
-
+    /**
+     * 添加好友，无分组
+     * @return
+     */
     public boolean addUser(String userName, String name){
         try {
             Roster roster = Roster.getInstanceFor(getConnection());
@@ -261,7 +302,10 @@ public class SmackClient {
 
         return false;
     }
-
+    /**
+     * 添加好友，有分组
+     * @return
+     */
     public boolean addUser(String userName, String name, String groupName){
         try {
             Presence presence = new Presence(Presence.Type.subscribe);
@@ -278,7 +322,10 @@ public class SmackClient {
 
         return false;
     }
-
+    /**
+     * 获取用户VCard信息
+     * @return
+     */
     public VCard getUserVcard(String name){
         VCard vCard = new VCard();
         try {
@@ -289,7 +336,10 @@ public class SmackClient {
 
         return vCard;
     }
-
+    /**
+     * 删除好友
+     * @return
+     */
     public boolean removeUser(String name){
         Roster roster = Roster.getInstanceFor(getConnection());
 
@@ -309,9 +359,12 @@ public class SmackClient {
 
         return false;
     }
-
+    /**
+     * 查询用户
+     * @return
+     */
     public List<HashMap<String, String>> searchUser(String name){
-        HashMap<String, String> user = new HashMap<String, String>();
+        HashMap<String, String> user = null;
         List<HashMap<String, String>> results = new ArrayList<HashMap<String, String>>();
         try {
             ServiceDiscoveryManager.getInstanceFor(getConnection());
@@ -321,12 +374,27 @@ public class SmackClient {
             Form answerForm = searchForm.createAnswerForm();
             answerForm.setAnswer("userAccount", true);
             answerForm.setAnswer("userPhote", name);
+            ReportedData data = usm.getSearchResults(answerForm, "search"+getConnection().getServiceName());
+
+            List<ReportedData.Row> rowList = data.getRows();
+            Iterator<ReportedData.Row> i = rowList.iterator();
+            ReportedData.Row row = null;
+            while (i.hasNext()){
+                user = new HashMap<String, String>();
+                row = i.next();
+                user.put("userAccount", row.getValues("userAccount").toString());
+                user.put("userPhote", row.getValues("userPhote").toString());
+                results.add(user);
+            }
         }catch (Exception e){
             e.printStackTrace();
         }
         return results;
     }
-
+    /**
+     * 修改心情
+     * @return
+     */
     public void changeStateMessage(String state){
         Presence presence = new Presence(Presence.Type.available);
         presence.setStatus(state);
@@ -337,7 +405,10 @@ public class SmackClient {
             e.printStackTrace();
         }
     }
-
+    /**
+     * 修改用户头像
+     * @return
+     */
     public boolean changeImage(File file){
         VCard vCard = new VCard();
         try {
@@ -358,7 +429,10 @@ public class SmackClient {
 
         return false;
     }
-
+    /**
+     * 文件转字节
+     * @return
+     */
     public byte[] getFileBytes(File file) throws IOException{
         BufferedInputStream bis = null;
         try {
@@ -376,7 +450,10 @@ public class SmackClient {
             }
         }
     }
-
+    /**
+     * 删除当前用户
+     * @return
+     */
     public boolean deleteAccount(){
         try {
             AccountManager accountManager = AccountManager.getInstance(getConnection());
@@ -387,7 +464,10 @@ public class SmackClient {
             return false;
         }
     }
-
+    /**
+     * 修改密码
+     * @return
+     */
     public boolean changePassword(String password){
         try {
             AccountManager accountManager = AccountManager.getInstance(getConnection());
@@ -396,6 +476,132 @@ public class SmackClient {
         }catch (Exception e){
             e.printStackTrace();
             return false;
+        }
+    }
+
+    /**
+     * 初始化会议室列表
+     * @return
+     */
+    public List<HostedRoom> getHostRooms(){
+        List<HostedRoom> roominfos = new ArrayList<HostedRoom>();
+        Collection<HostedRoom> hostedRooms = null;
+        try {
+            ServiceDiscoveryManager.getInstanceFor(getConnection());
+            hostedRooms = MultiUserChatManager.getInstanceFor(getConnection()).getHostedRooms(getConnection().getServiceName());
+            for(HostedRoom entry: hostedRooms){
+                roominfos.add(entry);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return roominfos;
+    }
+
+    /**
+     * 创建房间
+     * @param user
+     * @param roomName
+     * @param password
+     * @return
+     */
+    public MultiUserChat createRoom(String user, String roomName, String password){
+        MultiUserChat muc = null;
+        try {
+            muc = MultiUserChatManager.getInstanceFor(getConnection())
+                    .getMultiUserChat(roomName + "@conference" + getConnection().getServiceName());
+            muc.create(roomName);
+
+            Form form = muc.getConfigurationForm();
+            Form submitForm = form.createAnswerForm();
+            List<FormField> formFields = form.getFields();
+            Iterator<FormField> fields = formFields.iterator();
+            while (fields.hasNext()){
+                FormField field = (FormField)fields.next();
+                if(!FormField.FORM_TYPE.equals(field.getType()) && field.getVariable()!=null){
+                    submitForm.setDefaultAnswer(field.getVariable());
+                }
+            }
+
+            List<String> owners = new ArrayList<String>();
+            owners.add(getConnection().getUser());
+            submitForm.setAnswer("muc#roomconfig_roomowners", owners);
+            // 设置聊天室是持久聊天室，即将要被保存下来
+            submitForm.setAnswer("muc#roomconfig_persistentroom", true);
+            // 房间仅对成员开放
+            submitForm.setAnswer("muc#roomconfig_membersonly", false);
+            // 允许占有者邀请其他人
+            submitForm.setAnswer("muc#roomconfig_allowinvites", true);
+            if (!password.equals("")) {
+                // 进入是否需要密码
+                submitForm.setAnswer("muc#roomconfig_passwordprotectedroom",
+                        true);
+                // 设置进入密码
+                submitForm.setAnswer("muc#roomconfig_roomsecret", password);
+            }
+            // 能够发现占有者真实 JID 的角色
+            // submitForm.setAnswer("muc#roomconfig_whois", "anyone");
+            // 登录房间对话
+            submitForm.setAnswer("muc#roomconfig_enablelogging", true);
+            // 仅允许注册的昵称登录
+            submitForm.setAnswer("x-muc#roomconfig_reservednick", true);
+            // 允许使用者修改昵称
+            submitForm.setAnswer("x-muc#roomconfig_canchangenick", false);
+            // 允许用户注册房间
+            submitForm.setAnswer("x-muc#roomconfig_registration", false);
+            muc.sendConfigurationForm(submitForm);
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+        return muc;
+    }
+
+    /**
+     * 加入会议室
+     * @param user
+     * @param roomsName
+     * @param password
+     * @return
+     */
+    public MultiUserChat joinMultiUserChat(String user, String roomsName, String password){
+        try {
+            MultiUserChat muc = MultiUserChatManager.getInstanceFor(getConnection())
+                    .getMultiUserChat(roomsName + "@conference" + getConnection().getServiceName());
+            DiscussionHistory history = new DiscussionHistory();
+            history.setMaxChars(0);
+            muc.join(user, password, history, SmackConfiguration.getDefaultPacketReplyTimeout());
+            return muc;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 查询会议室成员名字
+     * @param muc
+     * @return
+     */
+    public List<String> findMultiUser(MultiUserChat muc){
+        List<String> listUser = muc.getOccupants();
+        return listUser;
+    }
+
+    /**
+     * 传送文件
+     * @param user
+     * @param filePath
+     */
+    public void sendFile(String user, String filePath){
+        FileTransferManager manager = FileTransferManager.getInstanceFor(getConnection());
+        OutgoingFileTransfer transfer = manager.createOutgoingFileTransfer(user);
+
+        try {
+            transfer.sendFile(new File(filePath), "You won't believe this");
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
